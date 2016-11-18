9f08cb94c18f11bf2bd41383e8ad142dde88fb06
JohnMorris4 <johnmorris@morrisje.com>
2016-11-18 07:32:49 -0500

    //moved files out of main index files

---
+++ includes/footer.php
@@ -0,0 +1,23 @@
+<!-- Footer -->
+        <footer>
+            <div class="row">
+                <div class="col-lg-12">
+                    <h4>Copyright &copy;<?php echo date("Y");?> Morris Web Design </h4>
+                </div>
+                <!-- /.col-lg-12 -->
+            </div>
+            <!-- /.row -->
+        </footer>
+
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,29 @@
+<?php
+
+include('includes/header.php')
+
+
+?>
+
+
+    <!-- Navigation -->
+    <?php include("includes/nav.php"); ?>
+    <!-- Page Content -->
+    <div class="container">
+
+        <div class="row">
+
+            <!-- Blog Entries Column -->
+           <?php include("includes/blogEntry.php") ?>
+
+            <!-- Blog Sidebar Widgets Column -->
+           <?php include("includes/sidebar.php") ?>
+
+        </div>
+        <!-- /.row -->
+
+        <hr>
+
+        <?php
+    include("includes/footer.php")
+    ?>
